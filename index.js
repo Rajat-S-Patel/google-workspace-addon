a15e18134416c16ba811ada6c618386e1e6700f5
Rajat Patel <patelrajat2000@gmail.com>
2023-08-25 18:17:00 +0530

    added console statement

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+    console.log('root called');
   res.send('Hello World!')
 })
 app.get('/home',(req,res) => {
