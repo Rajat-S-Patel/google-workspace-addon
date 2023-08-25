2d656a2074f91b1dcf9bb0ec1e4651d426626fa2
Rajat Patel <patelrajat2000@gmail.com>
2023-08-25 12:18:34 +0530

    added home route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/home',(req,res) => {
+    console.log('home called');
+    res.send('Home called');
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
