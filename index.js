b8e93c8d304a6b92d437e4c6647c1a73a3b7741b
Rajat Patel <patelrajat2000@gmail.com>
2023-08-28 10:42:38 +0530

    added basic card details

--- index.js
+++ index.js
@@ -15,13 +15,29 @@ app.get('/home',(req,res) => {
 app.post('/home',(req,res) => {
     console.log('home called post');
     let card = {
+      header: {
+        title:"Oreka RMS",
+        subtitle:"Spreadsheet Data",
+        imageUrl: "https://google-workspace-addon.onrender.com/oreka-logo-50.png",
+        imageType:"SQUARE",
+        imageAltText:"Oreka-Logo"
+      },
       sections: [{
         widgets: [{
           textParagraph: {
             text: 'Welcome to Oreka RMS'
           }
         }]
-      }]
+      }],
+      cardActions: {
+        actionLabel:"View Details",
+        onClick: {
+          action: {
+            function: "handleCardAction"
+          }
+        }
+      },
+      name:"Home Card"
     };
     res.json({
       action: {
